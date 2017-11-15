c06a8378052449e12deb0fbf5919a1f43e2da747
Chen Zhi <scaitong@gmail.com>
2017-11-15 18:00:34 +0800

    Add a fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -43,7 +43,7 @@ def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
         run(f'python3.6 -m venv {virtualenv_folder}')
-    run(f'{virutalenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
+    run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
 
 def _update_static_files(source_folder):
     run(
