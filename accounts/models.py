9b563be887dc9e8a35272f6294c6811486a21525
Chen Zhi <scaitong@gmail.com>
2018-02-12 22:20:50 -0500

    at the end of chap 19

--- accounts/models.py
+++ accounts/models.py
@@ -14,9 +14,9 @@ class User(models.Model):
     is_anonymous = False
     is_authenticated = True
 
+
 class Token(models.Model):
     email = models.EmailField()
     uid = models.CharField(default=uuid.uuid4, max_length=40)
-
 
 
